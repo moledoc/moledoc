fde538980375046596a5979eced10c9fc413a98a
meelis.utt <meelis.utt@gmail.com>
2022-02-06 20:33:10 +0200

    Single element list sorting test case

--- ADS/Sorting/insertion_test.go
+++ ADS/Sorting/insertion_test.go
@@ -19,8 +19,8 @@ func assertList(result []int, expected []int) bool {
 }
 
 func TestInsertion(t *testing.T) {
-	cases := [][]int{{3, 2, 1}, {-1, -2, -3}, {2, 1, 3}, {5, 6, 2, 3, 1, 10, 4, 7, 8, 9}, {}, {1, 2, 3, 4, 5}}
-	expected := [][]int{{1, 2, 3}, {-3, -2, -1}, {1, 2, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {}, {1, 2, 3, 4, 5}}
+	cases := [][]int{{3, 2, 1}, {-1, -2, -3}, {2, 1, 3}, {5, 6, 2, 3, 1, 10, 4, 7, 8, 9}, {}, {1, 2, 3, 4, 5}, {1}}
+	expected := [][]int{{1, 2, 3}, {-3, -2, -1}, {1, 2, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {}, {1, 2, 3, 4, 5}, {1}}
 	for i := 0; i < len(cases); i++ {
 		if !assertList(sort.Insertion(cases[i]), expected[i]) {
 			t.Fatalf("Expected %v, got %v\n", expected[i], cases[i])
