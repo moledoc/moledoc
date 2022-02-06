5f77b7276496ac13892edc0f39a5d091294dfacf
meelis.utt <meelis.utt@gmail.com>
2022-02-06 22:28:06 +0200

    ADS: sorting: renamed one insertion-sorting function (since it was inaccurate) and implemented decreasing insertion-sorting algorithm based on already implemented algorithm

--- ADS/sorting/insertion_test.go
+++ ADS/sorting/insertion_test.go
@@ -21,21 +21,32 @@ func assertList(result []int, expected []int) bool {
 
 var cases [][]int = [][]int{{3, 2, 1}, {-1, -2, -3}, {2, 1, 3}, {5, 6, 2, 3, 1, 10, 4, 7, 8, 9}, {}, {1, 2, 3, 4, 5}, {1}}
 var expected [][]int = [][]int{{1, 2, 3}, {-3, -2, -1}, {1, 2, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {}, {1, 2, 3, 4, 5}, {1}}
+var expectedDesc [][]int = [][]int{{3, 2, 1}, {-1, -2, -3}, {3, 2, 1}, {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, {}, {5, 4, 3, 2, 1}, {1}}
 
 func TestInsertion(t *testing.T) {
 	fmt.Print("Increasing algorithm: ")
 	for i := 0; i < len(cases); i++ {
-		if !assertList(sort.Insertion(cases[i]), expected[i]) {
+		if sorted := sort.Insertion(cases[i]); !assertList(sorted, expected[i]) {
 			fmt.Println("FAIL")
-			t.Fatalf("%v: Expected %v, got %v\n", i, expected[i], cases[i])
+			t.Fatalf("%v: Expected %v, got %v\n", i, expected[i], sorted)
 		}
 	}
 	fmt.Println("OK")
+
+	fmt.Print("Increasing algorithm 2: ")
+	for i := 0; i < len(cases); i++ {
+		if sorted := sort.Insertion(cases[i]); !assertList(sorted, expected[i]) {
+			fmt.Println("FAIL")
+			t.Fatalf("%v: Expected %v, got %v\n", i, expected[i], sorted)
+		}
+	}
+	fmt.Println("OK")
+
 	fmt.Print("Decreasing algorithm: ")
 	for i := 0; i < len(cases); i++ {
-		if !assertList(sort.InsertionDesc(cases[i]), expected[i]) {
+		if sorted := sort.InsertionDesc(cases[i]); !assertList(sorted, expectedDesc[i]) {
 			fmt.Println("FAIL")
-			t.Fatalf("%v: Expected %v, got %v\n", i, expected[i], cases[i])
+			t.Fatalf("%v: Expected %v, got %v\n", i, expected[i], sorted)
 		}
 	}
 	fmt.Println("OK")
